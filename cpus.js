fcce6a589acf25f505fd1aab4633c6476fa9ad9a
jellyfishgh <1129088994@qq.com>
2016-06-18 21:29:32 +0800

    bugfix cpus.js

--- cpus.js
+++ cpus.js
@@ -1,21 +1,22 @@
 var cluster = require('cluster');
 var http = require('http');
-var cups = require('os').cpus();
+var numCpus = require('os').cpus().length;
 
 var rssWarn = (50 * 1024 * 1024),
     heapWarn = (50 * 1024 * 1024);
 
 var workers = {};
 
-if(cluster.isMater){
-    for(var i = 0; i < cpus.lengths; i++){
+if (cluster.isMaster) {
+    console.log(`Master: ${process.pid}`);
+    for (var i = 0; i < numCpus; i++) {
         createWorker();
     }
-    setInterval(function(){
+    setInterval(function() {
         var time = new Date().getTime();
-        for(var pid in workers){
-            if(workers.hasOwnProperty(pid) && workers[pid].lastCb + 5000 < time){
-                console.log('Long running worker ' + pid + 'killed');
+        for (var pid in workers) {
+            if (workers.hasOwnProperty(pid) && workers[pid].lastCb + 5000 < time) {
+                console.log('Long running worker ' + pid + ' killed');
                 workers[pid].worker.kill();
                 delete workers[pid];
                 createWorker();
@@ -25,38 +26,37 @@ if(cluster.isMater){
     cluster.on('exit', (worker, code, signal) => {
         console.log(`worker ${worker.process.pid} died`);
     });
-    console.log(`Master: ${process.pid}`);
-} else{
-    http.createServer(function(req, res){
+} else {
+    http.createServer(function(req, res) {
         // 1/4 的概率超时
-        if(Math.floor(Math.random() * 4) === 1){
+        if (Math.floor(Math.random() * 4) === 1) {
             console.log('Stopped ' + process.pid + ' from ever finishing');
-            while(true){}
+            while (true) {}
         }
         res.writeHead(200);
         res.end('hello world from ' + process.pid + '\n');
     }).listen(8000);
-    // setInterval(function(){
-    //     process.send({
-    //         cmd: 'reportMem',
-    //         memory: process.memoryUsage(),
-    //         process: process.pid
-    //     });
-    // }, 1000);
+    setInterval(function(){
+        process.send({
+            cmd: 'reportMem',
+            memory: process.memoryUsage(),
+            process: process.pid
+        });
+    }, 1000);
 }
 
-function createWorker(){
+function createWorker() {
     var worker = cluster.fork();
-    console.log("Worker is created: " + worker.pid);
-    workers[worker.pid] = {
+    console.log("Worker is created: " + worker.process.pid);
+    workers[worker.process.pid] = {
         worker: worker,
-        lastCb: newDate().getTime() - 1000
+        lastCb: new Date().getTime() - 1000
     };
-    worker.on('message', function(msg){
-        if(msg.cmd === 'reportMem'){
-            workers[m.process].lastCb = new Date().getTime();
-            if(m.memory.rss > rssWarn){
-                console.log('Worker ' + m.process + 'using too much memory.');
+    worker.on('message', function(msg) {
+        if (msg.cmd === 'reportMem') {
+            workers[msg.process].lastCb = new Date().getTime();
+            if (msg.memory.rss > rssWarn) {
+                console.log('Worker ' + msg.process + 'using too much memory.');
             }
         }
     });
